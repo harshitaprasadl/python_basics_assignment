cf24b8c2780f8b2c32d3c3f490f2d9c01ad5a87c
harshitaprasadl <harshitaprasadl674@gmail.com>
2026-04-07 23:24:04 +0530

    Add files via upload

---
+++ pythonbasicsharshita.py
@@ -0,0 +1,21 @@
+# Task 1
+name = "Harshita"
+age = 22
+height = 1.57
+is_student = True
+
+print(name, age, height, is_student)
+
+# Task 2
+print(f"Name: {name} | Age: {age} | Height: {height} | Student: {is_student}")
+
+# Task 3
+age_in_months = age * 12
+age_in_days = age * 365
+remainder = age % 7
+age_square = age ** 2
+
+print("Age in months:", age_in_months)
+print("Age in days:", age_in_days)
+print("Remainder when age is divided by 7:", remainder)
+print("Age squared:", age_square)
